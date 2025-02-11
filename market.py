62ca38df0de9a537ca4304b11aa2fcf7bb72b37e
Sarah Sameh Wahba <117436397+SarahSameh19@users.noreply.github.com>
2025-02-11 09:48:53 +0200

    Styling & Templates, Sending data to Template, Template Inheritance

--- market.py
+++ market.py
@@ -1,11 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    name = db.Column(db.String(length=30))
+    price = db.Column(db.Float())
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
 
-@app.route("/about/<username>") #dynamic routes
-def about_page(username):
-    return f"<h1>This is the about page of {username}</h1>"
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode':'897527993625', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode':'123456789790', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode':'987654321098', 'price': 150},
+    ]
+    return render_template('market.html', items=items)
