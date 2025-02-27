d64cdfb8109e61fdb877fef028636b3399f79c68
Sarah Sameh Wahba <117436397+SarahSameh19@users.noreply.github.com>
2025-02-27 15:03:43 +0200

    Flask Validations

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,9 @@
 from market import app
-from flask import render_template
-from market.models import Item 
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
+from flask import request  # Make sure to import request
 
 @app.route("/")
 @app.route("/home")
@@ -13,8 +15,21 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+        
+    if form.validate_on_submit():
+        print(f"Username Field Data from Form: {form.username.data}")  # What Flask-WTF sees
+        user_to_create = User(
+                            username= form.username.data,
+                            email_address=form.email_address.data,
+                            password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))  
+    if form.errors != {}: #if there are not errors from the validations
+        for err_msg in form.errors.values():
+            print(f'there was an error with creating a user: {err_msg}') #print error message
 
     return render_template('register.html', form=form)
